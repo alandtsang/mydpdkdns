8049d78a7b0edc37a0c4178e5889e5a4f6dd609b
Aland <zengxianglong0@gmail.com>
2017-08-15 20:56:07 +0800

    Added ethrecord.sh and updated upeth.sh

--- tools/upeth.sh
+++ tools/upeth.sh
@@ -1,3 +1,18 @@
-ifconfig vEth0 192.168.44.129 netmask 255.255.255.0
-ifconfig vEth0 hw ether 00:0c:29:75:1e:19
-#route add default gw 192.168.44.2
+#!/bin/bash
+
+basepath=$(cd `dirname $0`; pwd)
+nicinfo="$basepath/nicinfo.log"
+dev="vEth0"
+
+if [ ! -f "$nicinfo" ]; then
+   echo "Get Network Config Failed" && exit 1
+else
+    IPADDR=`grep 'IPADDR' $nicinfo | cut -d "=" -f2`
+    NETMASK=`grep 'NETMASK' $nicinfo | cut -d "=" -f2`
+    MAC=`grep 'MAC' $nicinfo | cut -d "=" -f2`
+    GATEWAY=`grep 'GATEWAY' $nicinfo | cut -d "=" -f2`
+fi
+
+ifconfig $dev $IPADDR netmask $NETMASK
+ifconfig $dev hw ether $MAC
+#route add default gw $GATEWAY
